cf63f05dfbc1b5e2153e8502d7e970f824818c86
Utcka228 <137770082+Utcka228@users.noreply.github.com>
2024-09-23 16:33:54 +0300

    Add files via upload

---
+++ php/send.php
@@ -0,0 +1,46 @@
+<?php
+
+$name = $_POST["name"];
+$ear = $_POST["ear"];
+$tel = $_POST["tel"];
+
+$name = htmlspecialchars($name)
+$ear = htmlspecialchars($ear)
+$tel = htmlspecialchars($tel)
+
+$name = urlencode($name)
+$ear = urlencode($ear)
+$tel = urlencode($tel)
+
+$name = trim($name)
+$ear = trim($ear)
+$tel = trim($tel)
+
+if (mail)("matvey228yh8@mail.ru"
+    "Новое письмо с сайта Програмирование"
+    "Имя: ".$name."\n".
+    "Возраст: ".$ear."\n".
+    "Телефон: ".$tel."\n".)
+
+    {
+        echo ("Письмо успешно отправлено.")
+    }
+
+    else {
+        echo ("Есть ошибки! Проверьте данные.")
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
