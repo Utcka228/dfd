7033bb009aac7637c2500dcf53b9e4d6fec1e28a
Utcka228 <137770082+Utcka228@users.noreply.github.com>
2024-09-23 17:50:31 +0300

    Update send.php

--- php/send.php
+++ php/send.php
@@ -1,23 +1,23 @@
 <?php
 
-$name = $_POST["name"];
-$ear = $_POST["ear"];
-$tel = $_POST["tel"];
+ $name = $_POST["name"];
+ $ear = $_POST["ear"];
+ $tel = $_POST["tel"];
 
-$name = htmlspecialchars($name)
-$ear = htmlspecialchars($ear)
-$tel = htmlspecialchars($tel)
+ $name = htmlspecialchars($name)
+ $ear = htmlspecialchars($ear)
+ $tel = htmlspecialchars($tel)
 
-$name = urlencode($name)
-$ear = urlencode($ear)
-$tel = urlencode($tel)
+ $name = urlencode($name)
+ $ear = urlencode($ear)
+ $tel = urlencode($tel)
 
-$name = trim($name)
-$ear = trim($ear)
-$tel = trim($tel)
+ $name = trim($name)
+ $ear = trim($ear)
+ $tel = trim($tel)
 
-if (mail("matvey228yh8@mail.ru"
-    "Новое письмо с сайта Програмирование"
+ if (mail("matvey228yh8@mail.ru",
+    "Новое письмо с сайта Програмирование",
     "Имя: ".$name."\n".
     "Возраст: ".$ear."\n".
     "Телефон: ".$tel."\n".
